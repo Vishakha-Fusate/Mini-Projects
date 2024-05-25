afaafb681e18a792011bcd687157dc22197604c4
vishakha <vishakha0899@gmail.com>
2024-05-25 16:45:47 +0530

    recaptcha file added

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+function beforesubmit() {
+  let outputDate = document.querySelector(".outputDate");
+  let inputDate = document.querySelector(".inputdate");
+  //console.log(inputDate.value);
 
-function beforesubmit(){
-    let outputDate =  document.querySelector(".outputDate");
-    let inputDate = document.querySelector(".inputdate");
-    //console.log(inputDate.value);
-    
-    //formatting type string date to date (en_IN) locale
-    let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
-    outputDate.value = formattedDate;
-    
+  //formatting type string date to date (en_IN) locale
+  let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
+  outputDate.value = formattedDate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
